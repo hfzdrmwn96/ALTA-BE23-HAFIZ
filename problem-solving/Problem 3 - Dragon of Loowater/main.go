5bc7672fdad7187e2e35156c1d677504945c01f1
Hafiz Darmawan <hafizdarmawan1996@gmail.com>
2024-06-02 15:36:29 +0700

    revisi problem 3 v2

--- problem-solving/Problem 3 - Dragon of Loowater/main.go
+++ problem-solving/Problem 3 - Dragon of Loowater/main.go
@@ -7,16 +7,16 @@ import (
 
 func DragonOfLoowater(dragonHead, knightHeight []int) {
 	//code
-	var result [2]int
+	var result = [2]int{len(dragonHead)}
 	var isFall bool = false
 	sort.Ints(knightHeight)
 	sort.Ints(dragonHead)
 	for _, dh := range dragonHead {
-		result[0] += dh
 		for i := 0; i < len(knightHeight); i++ {
 			if dh <= knightHeight[i] {
 				result[1] += knightHeight[i]
 				knightHeight = append(knightHeight[:i], knightHeight[i+1:]...)
+				result[0]--
 				// fmt.Println(knightHeight)
 				break
 			} else if i == len(knightHeight)-1 {
@@ -28,7 +28,7 @@ func DragonOfLoowater(dragonHead, knightHeight []int) {
 		}
 	}
 
-	if !isFall && result[1] > result[0] {
+	if !isFall && result[0] == 0 {
 		fmt.Println(result[1])
 	} else {
 		fmt.Println("knight fall")
@@ -37,7 +37,7 @@ func DragonOfLoowater(dragonHead, knightHeight []int) {
 
 func main() {
 	DragonOfLoowater([]int{5, 4}, []int{7, 8, 4})                                        // 11
-	DragonOfLoowater([]int{5, 10}, []int{5})                                             // knight fall
+	DragonOfLoowater([]int{5, 10}, []int{20})                                            // knight fall
 	DragonOfLoowater([]int{7, 2}, []int{4, 3, 1, 2})                                     // knight fall
 	DragonOfLoowater([]int{7, 2, 3, 4, 5, 6, 4}, []int{7, 5, 4, 2, 2, 1, 4, 3, 1, 2, 8}) // 33
 }
